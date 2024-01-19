8f1c9a74870ec62101942a1f8a182b02025d37dc
Ebenezer terabo <teraboebenezer@gmail.com>
2024-01-19 01:05:58 +0000

    Task 1 solved

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import calculator_1 as cal
+
+if __name__ == "__main__":
+
+    a = 10
+    b = 5
+
+    add = cal.add(a, b)
+    sub = cal.sub(a, b)
+    mul = cal.mul(a, b)
+    div = cal.div(a, b)
+
+    print("{} + {} = {}".format(a, b, add))
+    print("{} - {} = {}".format(a, b, sub))
+    print("{} * {} = {}".format(a, b, mul))
+    print("{} / {} = {}".format(a, b, div))
