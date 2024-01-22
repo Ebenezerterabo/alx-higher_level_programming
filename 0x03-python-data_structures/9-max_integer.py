e2b6d1f3fe5f22b9eb071a7bfca816a119552158
Ebenezer terabo <teraboebenezer@gmail.com>
2024-01-22 17:47:43 +0000

    Task 9 solved

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max_num = 0
-    if not my_list[]:
+    if not my_list:
         return None
-    else:
-        for element in my_list:
-            if element > max_num:
-                max_num = element
+
+    for element in my_list:
+        if element > max_num:
+            max_num = element
 
         return max_num
