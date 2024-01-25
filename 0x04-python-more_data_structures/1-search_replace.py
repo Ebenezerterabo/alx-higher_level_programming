14d8b9358f04130368dbe06efb04bb57d4e290e7
Ebenezer terabo <teraboebenezer@gmail.com>
2024-01-25 01:29:01 +0000

    Task 1 solved

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if search >= 1 and search >= len(my_list):
+        return my_list
+
     modified_list = [x for x in my_list]
     for i, num in enumerate(my_list):
         if i == search - 1:
