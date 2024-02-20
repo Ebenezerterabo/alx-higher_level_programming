01f0adaec5a2c75f3b9a52776fc430d5dca3ebfd
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-20 10:39:34 +0000

    Task 0 solved

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 
-"""
-Perform a lookup operation on the given object.
-
-Args:
-- obj: The object to perform the lookup operation on.
-
-Returns:
-- list: The result of the lookup operation.
+""" A module that returns the list of available attributes and methods
+of an object
 """
 
 
 def lookup(obj):
+    """
+    Perform a lookup operation on the given object.
+
+    Args:
+    - obj: The object to perform the lookup operation on.
+
+    Returns:
+    - list: The result of the lookup operation.
+    """
     return dir(obj)
