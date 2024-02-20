a880244d11e96a27eb05b2f1484cd893065acf99
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-20 03:21:20 +0000

    Task 0 solved

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Perform a lookup operation on the given object.
+
+Args:
+- obj: The object to perform the lookup operation on.
+
+Returns:
+- list: The result of the lookup operation.
+"""
+
+
+def lookup(obj):
+    return dir(obj)
