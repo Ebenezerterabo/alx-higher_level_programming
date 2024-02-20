a537b675e00f4c28fd0864be21ab1cd0190a226f
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-20 12:12:24 +0000

    Task 6 solved

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Base geometry class """
+
+
+class BaseGeometry:
+    """ A public method for area of geometry """
+    def area(self):
+        raise Exception("area() is not implemented")
