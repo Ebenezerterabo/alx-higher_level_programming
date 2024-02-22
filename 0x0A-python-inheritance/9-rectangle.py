08b166668dda5abeb41d40123acfba63ebbec1dd
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-22 02:04:46 +0000

    Task 9 solved

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,19 +1,5 @@
-#!/usr/bin/python3
-""" Base geometry class """
-
-
-class BaseGeometry:
-    """ A public method for area of geometry """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ An integer validator method """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
+""" Module 8-rectangle """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -37,4 +23,5 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
+        """ Returns a readable string """
         return f"[Rectangle] {self.__width}/{self.__height}"
