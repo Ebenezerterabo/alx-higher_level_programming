934ebb2ebc915dd9f2ddfc4ce3faf604e3070eb4
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-23 17:25:33 +0000

    Task 11 solved

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+This module is a class that defines student
+"""
+
+
+class Student:
+    """A class that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializing instance attribute"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of a
+        student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+
+        dictionary = {}
+        for key in attrs:
+            if key in self.__dict__:
+                dictionary.update({key: self.__dict__.get(key)})
+
+        return dictionary
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
