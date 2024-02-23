adbc00f9022f5642f47a7ae1d014089450d396e1
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-23 00:22:17 +0000

    Task 0 solved

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+"""This module reads and display the content of a file"""
 
 def read_file(filename=""):
     """
@@ -8,6 +9,6 @@ def read_file(filename=""):
     Args:
         filename (str): The name of the text file.
     """
-    with open("my_file_0.txt") as file:
+    with open("my_file_0.txt", encoding="utf-8") as file:
         content = file.read()
-        print(content)
+        print(content, end='')
