4872be0aff3a8a932c06b6b466a0606191b148cd
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-28 13:56:02 +0000

    Rectangle class modified str method added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,3 +85,7 @@ class Rectangle(Base):
         """Prints the structure of the Rectangle"""
         for _ in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
+                f" - {self.__width}/{self.__height}")
