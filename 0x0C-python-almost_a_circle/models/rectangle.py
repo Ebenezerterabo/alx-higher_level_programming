118eb8df19fb7a7bf5d596c7dd60eb1c694021fd
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-28 15:27:38 +0000

    Rectangle class modified update function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,5 +93,28 @@ class Rectangle(Base):
             print("#" * self.__width)
 
     def __str__(self):
+        """Return the readable string to the user"""
         return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
                 f" - {self.__width}/{self.__height}")
+
+    def update(self, *args):
+        """Assigns args to each attribute.
+
+        Args:
+           id (int): 1st argument
+           width (int): 2nd argument
+           height (int): 3rd argument
+           x (int): 4th argument
+           y (int): 5th argument
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
