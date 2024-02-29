5610605efbe2a2258dc79979a65e45c81ab35eae
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-29 02:06:08 +0000

    Square class(update method)

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,14 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize the attribute
+
+        Args:
+           size (int): The size of the square
+           x (int): The x-cordinate of the square
+           y (int): The y-cordinate of the square
+           id (int): The square identifier
+        """
         super().__init__(size, size, x, y, id)
 
     @property
@@ -18,6 +26,28 @@ class Square(Rectangle):
         """Sets the size of the Square"""
         self.width = size_value
         self.height = size_value
+
+    def update(self, *args, **kwargs):
+        """This method update the attributes
+
+        Args:
+           id (int): 1st argument
+           size (int): 2nd argument
+           x (int): 3rd argument
+           y (int): 4th argument
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def __str__(self):
         """Returns the string in readable format"""
