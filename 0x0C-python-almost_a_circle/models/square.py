f606323ab750816c9ebdbcef0958e2fca8677390
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-29 01:41:10 +0000

    Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,22 +7,18 @@ class Square(Rectangle):
     """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self._size = size
 
     @property
     def size(self):
         """Gets the size of the Square"""
-        return self._size
+        return self.width
 
     @size.setter
     def size(self, size_value):
         """Sets the size of the Square"""
-        if type(size_value) != int:
-            raise TypeError("width must be an integer")
-        elif size_value <= 0:
-            raise ValueError("width must be > 0")
-        self._size = size_value
+        self.width = size_value
+        self.height = size_value
 
     def __str__(self):
         """Returns the string in readable format"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self._size}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
