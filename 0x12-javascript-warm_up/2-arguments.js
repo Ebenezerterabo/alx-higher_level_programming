815f42913abb305550430ce1fce242f15a195e2b
Ebenezer terabo <teraboebenezer@gmail.com>
2024-04-22 22:23:45 +0000

    Task 2 solved

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 const numArgs = args.length;
 
 if (numArgs === 0) {
-  console.log('No Argument');
+  console.log('No argument');
 } else if (numArgs === 1) {
   console.log('Argument found');
 } else {
