dcfd80f31e8718e9f502f73b13bbaac9676e6ff6
Ebenezer terabo <teraboebenezer@gmail.com>
2024-04-25 01:46:42 +0000

    Task 8 solved

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@ exports.esrever = function (list) {
   const midPoint = Math.floor(list.length / 2);
 
   for (let i = 0; i < midPoint; i++) {
-    let temp = list[i];
+    const temp = list[i];
     list[i] = list[list.length - 1 - i];
     list[list.length - 1 - i] = temp;
   }
