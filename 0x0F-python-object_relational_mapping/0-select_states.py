dcd7cbc964f07be1383a1c6639f4c1b9d6578efc
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-04-27 17:33:03 +0000

    deleted

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+""" a script that lists all states
+from the database hbtn_0e_0_usa """
+
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    """Main program"""
     db = MySQLdb.connect(
         user=argv[1],
         password=argv[2],
