1dd65e07d39b4b8ea975d1e6b2f712c9049349d4
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-05-03 23:32:50 +0000

    Task 14 solved

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,9 +4,11 @@
 from model_state import Base
 from sqlalchemy import Integer, String, Column, ForeignKey
 
+
 class City(Base):
     """ Structure linked to my sql table """
     __tablename__ = "cities"
-    id = Column(Integer, nullable=False, autoincrement=True, unique=True, primary_key=True)
+    id = Column(Integer, nullable=False, autoincrement=True,
+                unique=True, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
