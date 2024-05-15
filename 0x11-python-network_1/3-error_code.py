a94187f31d83b818627ffd7681b1f8ca448a5f07
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-05-15 17:40:40 +0000

    Task 3 solved

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,13 +4,14 @@ A script that takes in a URL, sends a request to the URL and displays
 the body of the response
 """
 
-from urllib.request import urlopen
-import urllib.error
-from sys import argv
+if __name__ == "__main__":
+    from urllib.request import urlopen
+    import urllib.error
+    from sys import argv
 
-try:
-    with urlopen(argv[1]) as response:
-        data = response.read()
-        print(f'{data.decode('UTF-8')}')
-except urllib.error.HTTPError as e:
-    print(f'Error code: {e.code}')
+    try:
+        with urlopen(argv[1]) as response:
+            data = response.read()
+            print(f'{data.decode('UTF-8')}')
+    except urllib.error.HTTPError as e:
+        print(f'Error code: {e.code}')
