eabb84ce21c93cf484b6a30976b6ed01f0d9c614
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-05-15 23:30:21 +0000

    Task 8 solved

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 A script that takes in a letter and sends a POST request to url with
 letter as parameter
@@ -10,16 +11,17 @@ from sys import argv
 if __name__ == "__main__":
     # Post request
     url = "http://0.0.0.0:5000/search_user"
-    
+
     if len(argv) == 2:
         q = argv[1]
     else:
         q = ""
-    
-    response = requests.post(url, data={'q':q})
+
+    response = requests.post(url, data={'q': q})
     try:
-        if response.json():
-            print(f'[{response.json().get('id')}] {response.json().get('name')}')
+        json_response = response.json()
+        if json_response:
+            print(f'[{json_response.get('id')}] {json_response.get('name')}')
         else:
             print('No result')
     except ValueError:
