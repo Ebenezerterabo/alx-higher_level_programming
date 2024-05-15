0848f09e785b8ff4ad4cb0d48423838e1d95540e
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-05-15 22:27:45 +0000

    Task 7 solved

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -13,5 +13,8 @@ if __name__ == "__main__":
     response = requests.get(argv[1])
     # Get the response status codde
     error_code = response.status_code
-    # Print the error code
-    print(f'Error code: {error_code}')
+    # Check and Print the status code
+    if error_code == 200:
+        print("Index")
+    else:
+        print(f'Error code: {error_code}')
