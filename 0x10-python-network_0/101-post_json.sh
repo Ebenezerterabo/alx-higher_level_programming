d9d4b96137b7fc524ef17e7eedf6d8e8bc10752a
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-05-15 05:42:02 +0000

    Advance task 2

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that sends a JSON POST request to a URL and displays body as response
-curl -X POST -H "content-Type: application/json" -d "(cat "$2")" "$1"
+curl -X POST -sH "content-Type: application/json" -d "("cat "$2")" "$1"
